4eb57965d6ae6f0217bf6793a34b126748a497a3
saviganga <akatugbasaviour@gmail.com>
2021-05-13 03:53:30 -0700

    changed import style for temp_converter

--- tests.py
+++ tests.py
@@ -1,16 +1,15 @@
 import unittest
-
-from temp_converter import c_to_f, f_to_c
+import temp_converter
 
 class Tests(unittest.TestCase):
 
     #check c_to_f returns correct values
     def test_c_to_f(self):
-        self.assertEqual(212, c_to_f(100))
+        self.assertEqual(212, temp_converter.c_to_f(100))
 
     #check f_to_c returns correct value
     def test_f_to_c(self):
-        self.assertEqual(0, f_to_c(32))
+        self.assertEqual(0, temp_converter.f_to_c(32))
 
 if __name__ == '__main__':
     unittest.main()
