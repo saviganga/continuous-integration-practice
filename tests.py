b4a86db0bdf5bece6f4f71622f6faa7087b6fc04
saviganga <akatugbasaviour@gmail.com>
2021-05-13 16:23:00 -0700

    km to m tests in tests.py

--- tests.py
+++ tests.py
@@ -1,18 +1,20 @@
 import unittest
 import temp_converter
-
+import distance_converer
 
 class Tests(unittest.TestCase):
 
     # check c_to_f returns correct values
     def test_c_to_f(self):
-        self.assertEqual(212, temp_converter.c_to_f(100))
+        self.assertEqual(20.5, temp_converter.c_to_f(68.9))
 
     # check f_to_c returns correct value
     def test_f_to_c(self):
 
         self.assertEqual(0, temp_converter.f_to_c(32))
 
+    def test_km_to_m(self):
+        self.assertEqual(3, distance_converter(3000))
 
 if __name__ == '__main__':
     unittest.main()
